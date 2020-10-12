a67e690b68ef0204bcb0e27ca8ceafcb6f1d8b39
Trevor2492 <69220176+Trevor2492@users.noreply.github.com>
2020-10-11 23:00:32 -0500

    Added routing functionality
    Added routing logic and messaging with Express

--- index.js
+++ index.js
@@ -55,13 +55,57 @@ app.use((err, req, res, next) => { //This is the error handling function
   res.status(500).send('Something broke');
 });
 
-// GET requests
+
+// DIFFERENT REQUEST TYPES AND THEIR METHODS
+// This shows the default message
 app.get('/', (req, res) => {
   res.send('This is the default textual response page');
 });
 
+// This shows a list of all the movie objects
 app.get('/movies', (req, res) => {
-  res.json(topMovies);
+  res.json(topMovies)
+});
+
+// This shows the details for a movie based on the movie name
+app.get('/movies/:title', (req, res) => {
+  res.json(topMovies.find((movie) =>
+  { return movie.title === req.params.title }));
+});
+
+// This shows information about a specific genre(description) by name/title
+app.get('/movies/genres/genre', (req, res) => {
+  res.send('This is where we will show more information about a specific genre');
+});
+
+// This shows information about a specific director by name
+app.get('/movies/Directors/director', (req, res) => {
+  res.send('This is where we will show more information about a specific Director');
+});
+
+// Registers a new user
+app.post('/users', (req, res) => {
+  res.send('This is where a new user will be able to register');
+});
+
+// This allows the user to update their info
+app.put('/users/username', (req, res) => {
+  res.send('This is where the user will be able to update their information');
+});
+
+// This allows the user to add a movie to their list
+app.post('/users/username/movies/movieID', (req, res) => {
+  res.send('This is where users can add a movie to their list of favorites');
+});
+
+// This allows the user to delete a movie from their list
+app.delete('/users/username/movies/movieID', (req, res) => {
+  res.send('This is where users will be able to delete a movie from their list');
+});
+
+// This deregisters the user
+app.delete('/users/username', (req, res) => {
+  res.send('This is where users will be able to deregister');
 });
 
 // listen for requests
