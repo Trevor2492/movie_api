6a8416de80285dba851034374011d5055dd3b2e3
Trevor2492 <69220176+Trevor2492@users.noreply.github.com>
2020-10-23 12:43:38 -0500

    just some testing

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get('/', (req, res) => {
 });
 
 // This shows a list of all the movie objects
-app.get('/movies', passport.authenticate('jwt', {session: false}), (req, res) => {
+app.get('/movies', (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
